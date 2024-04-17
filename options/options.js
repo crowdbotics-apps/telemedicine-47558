ba08372c77205cbba84808582c950667756b4001
Crowdbotics <team@crowdbotics.com>
2024-04-17 05:31:33 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "telemedicine-47558",
+  url: "https://telemedicine-47558.botics.co",
+  api: "https://telemedicine-47558.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
